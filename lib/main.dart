7b06380e98eebb847dc3bfae223a306b0e951f60
kshgrk <kushagra9nov@gmail.com>
2021-08-12 13:38:40 +0530

    ensured firebase to initialize app

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 //import 'package:simple_chat_app/screens/signin.dart';
 import 'package:simple_chat_app/screens/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:simple_chat_app/services/google_sign_in.dart';
 
-void main() {
+/*void main() {
+  runApp(MyApp());
+}*/
+
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+    create: (context) => GoogleSignInProvider(),
+    child : MaterialApp(
+        title: 'Neos Chat',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SignUp(),
       ),
-      home: SignUp(),
-    );
-  }
+  );
 }
 
